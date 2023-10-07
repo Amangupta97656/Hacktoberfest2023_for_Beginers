4825afeef65a9c3b98151e4520d93f34a50babc4
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-10-08 00:34:26 +0530

    Create Binary_tree.py

---
+++ Binary_tree.py
@@ -0,0 +1,83 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self, root):
+        self.root = TreeNode(root)
+
+    def insert(self, value):
+        self._insert_recursive(self.root, value)
+
+    def _insert_recursive(self, current, value):
+        if value < current.value:
+            if current.left is None:
+                current.left = TreeNode(value)
+            else:
+                self._insert_recursive(current.left, value)
+        elif value > current.value:
+            if current.right is None:
+                current.right = TreeNode(value)
+            else:
+                self._insert_recursive(current.right, value)
+
+    def search(self, value):
+        return self._search_recursive(self.root, value)
+
+    def _search_recursive(self, current, value):
+        if current is None:
+            return False
+        if current.value == value:
+            return True
+        elif value < current.value:
+            return self._search_recursive(current.left, value)
+        else:
+            return self._search_recursive(current.right, value)
+
+    def inorder_traversal(self):
+        result = []
+        self._inorder_recursive(self.root, result)
+        return result
+
+    def _inorder_recursive(self, node, result):
+        if node:
+            self._inorder_recursive(node.left, result)
+            result.append(node.value)
+            self._inorder_recursive(node.right, result)
+
+    def preorder_traversal(self):
+        result = []
+        self._preorder_recursive(self.root, result)
+        return result
+
+    def _preorder_recursive(self, node, result):
+        if node:
+            result.append(node.value)
+            self._preorder_recursive(node.left, result)
+            self._preorder_recursive(node.right, result)
+
+    def postorder_traversal(self):
+        result = []
+        self._postorder_recursive(self.root, result)
+        return result
+
+    def _postorder_recursive(self, node, result):
+        if node:
+            self._postorder_recursive(node.left, result)
+            self._postorder_recursive(node.right, result)
+            result.append(node.value)
+
+# Example usage:
+if __name__ == "__main__":
+    tree = BinaryTree(5)
+    tree.insert(3)
+    tree.insert(8)
+    tree.insert(1)
+    tree.insert(4)
+    tree.insert(7)
+    tree.insert(9)
+
+    print("Inorder Traversal:", tree.inorder_traversal())
+    print("Preorder Traversal:", tree.pre
